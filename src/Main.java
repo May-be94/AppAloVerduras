732fda71bc014fe6f9af7f9573e498ad77848100
Mayra Cardoso <110548147+May-be94@users.noreply.github.com>
2023-06-16 15:04:56 -0300

    ajustes

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,15 @@ public class Main {
     private List<Cliente> clientes = new ArrayList<>();
 
     public static void main(String[] args) {
-        Cliente cl1 = new Cliente("joao","joao@email.com","rua 1");
-        Cliente cl2 = new Cliente("maria","joao@email.com","rua 1");
-        Cliente cl3 = new Cliente("josa","joao@email.com","rua 1");
+        Cliente cl1 = new Cliente("Joao","joao@email.com","rua 1");
+        Cliente cl2 = new Cliente("Maria","maria@email.com","rua 2");
+        Cliente cl3 = new Cliente("Jose","joao@email.com","rua 3");
 
         Cadastro cadastro = new Cadastro();
 
         cadastro.cadastrarCliente(cl1,"123");
-        cadastro.cadastrarCliente(cl2,"123");
-        cadastro.cadastrarCliente(cl3,"123");
+        cadastro.cadastrarCliente(cl2,"345");
+        cadastro.cadastrarCliente(cl3,"678");
 
         cadastro.exibirClientes();
 
